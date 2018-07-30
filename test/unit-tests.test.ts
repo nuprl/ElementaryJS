35c50f2af7c0accdc5461984cef993f857691aa7
Sam Lee <chunghinlee@umass.edu>
2018-07-30 15:45:18 -0400

    updated unit tests with options

--- test/unit-tests.test.ts
+++ test/unit-tests.test.ts
@@ -1,12 +1,16 @@
 import { compile } from '../ts/index';
 import { sandbox } from '../ts/sandbox';
 import { default as generator } from 'babel-generator';
-import 'jest';
+
+const compileOptions = {
+  isOnline: false,
+  runTests: false,
+}
 
 // Helps write test cases that expect the program to terminate normally.
 // The result is the final value of the program.
 function run(code: string): any {
-  const result = compile(code, false);
+  const result = compile(code, compileOptions);
   if (result.kind === 'error') {
     throw result;
   }
@@ -23,7 +27,7 @@ function run(code: string): any {
 // A test case should check that the message is reasonable, or it could have
 // been some other kind of failure.
 function dynamicError(code: string): string {
-  const result = compile(code, false);
+  const result = compile(code, compileOptions);
   if (result.kind === 'error') {
     throw result;
   }
@@ -42,7 +46,7 @@ function dynamicError(code: string): string {
 // Helps write test cases that check for static errors. The result
 // is the array of error messages produced by ElementaryJS.
 function staticError(code: string): string[] {
-  const result = compile(code, false);
+  const result = compile(code, compileOptions);
   if (result.kind === 'ok') {
     throw new Error(`expected a static error, but none produced`);
   }
