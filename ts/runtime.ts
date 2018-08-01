244483dc39ab04b071e1642b9612054a5706e426
Sam Lee <chunghinlee@umass.edu>
2018-08-01 16:27:14 -0400

    Got rid of visitor code for tests, move testing runtime to runtime

--- ts/runtime.ts
+++ ts/runtime.ts
@@ -145,3 +145,84 @@ export function applyNumOp(op: string, lhs: any, rhs: any) {
     }
   }
 }
+
+let tests: {
+  failed: boolean,
+  description: string,
+  error: string,
+  miliElapsed: number
+}[] = [];
+
+var testsEnabled = false;
+
+export function enableTests(enable: boolean) {
+  testsEnabled = enable;
+}
+
+export function assert(val: boolean) {
+  if (typeof val !== 'boolean') {
+    throw new Error(`${val} is not a boolean value`);
+  }
+  if (!val) {
+    throw new Error(`Assertion failed`);
+  }
+
+  return true;
+}
+
+export function test(description: string, testFunction: () => void) {
+  const start = Date.now(); // I should use perfomance.now but I don't think it matters that much
+  try { // I just want some cool numbers showing up
+      testFunction();
+      const end = Date.now(); 
+      tests.push({
+          failed: false,
+          description: description,
+          error: '',
+          miliElapsed: end - start,
+      });
+  } catch (e) {
+      const end = Date.now();
+      tests.push({
+          failed: true,
+          description: description,
+          error: e.message,
+          miliElapsed: end - start,
+      });
+
+  }
+}
+
+export function summary() {
+  if (tests.length === 0) {
+      console.log(`%c◈ You don't seem to have any tests written`, 'color: #e87ce8');
+      console.log(`%c◈ To run a test, begin a function name with 'test'`, 'color: #e87ce8');
+      return;
+  }
+  let numPassed = 0;
+  let numFailed = 0;
+  let totalMili = 0;
+  for (let result of tests) {
+      totalMili += result.miliElapsed;
+      if (result.failed) {
+          console.log(
+              `%c FAILED %c ${result.description} (${result.miliElapsed.toFixed(0)}ms)\n${result.error}`,
+              'background-color: #f44336; font-weight: bold',
+              'color: inherit; background-color: inherit'
+          );
+          numFailed += 1;
+          continue;
+      }
+      console.log(
+          `%c OK %c ${result.description} (${result.miliElapsed.toFixed(0)}ms)`,
+          'background-color: #2ac093; font-weight: bold',
+          'color: inherit; background-color: inherit'
+      );
+      numPassed += 1;
+  }
+  console.log(`Tests:     %c${numFailed} failed, %c${numPassed} passed, %c${numPassed + numFailed} total`,
+  'color: #f44336; font-weight: bold', 'color: #2ac093; font-weight: bold', 'font-weight: bold'
+  );
+  console.log(`Time:      ${(totalMili / 1000).toFixed(2)}s`);
+  tests = [];
+}
--- ts/visitor.ts
+++ ts/visitor.ts
@@ -28,7 +28,6 @@
 import * as t from 'babel-types';
 import { Visitor, NodePath } from 'babel-traverse';
 import { ElementarySyntaxError, CompileError } from './types';
-import { testingFunctions } from './runtime-testing';
 
 let generalOperators = [
   "==",
@@ -113,13 +112,6 @@ function rtsExpression(st: S): t.Expression {
   }
 }
 
-function rtsTestingExpression(st: S) {
-  if (st.opts.isOnline) {
-    return t.identifier('celot');
-  }
-  return t.callExpression(t.identifier('require'), [t.stringLiteral('./runtime-testing')]);
-}
-
 function unassign(op: string) {
   switch (op) {
     case '+=': return '+';
@@ -173,56 +165,6 @@ export const visitor: Visitor = {
       if (st.elem.errors.length > 0) {
         throw st.elem;
       }
-      // --- Runtime testing stuff ---
-      if (!st.opts.runTests) {
-        return;
-      }
-      for (let i = 0; i < path.node.body.length; i++) {
-        const statement = path.node.body[i];
-        if (!t.isExpressionStatement(statement) || !t.isCallExpression(statement.expression)) {
-          continue;
-        }
-        if (t.isIdentifier(statement.expression.callee) &&
-          statement.expression.callee.name === 'test') {
-          statement.expression.callee = t.memberExpression(
-            t.identifier('rtsTest'),
-            t.identifier('test')
-          );
-        }
-      }
-
-      const runTimeVariableExpression = t.variableDeclaration('var', [
-        t.variableDeclarator(t.identifier('rtsTest'), rtsTestingExpression(st))
-      ]);
-
-      if (path.node.body.length !== 0) {
-        path.get('body.0').insertBefore(runTimeVariableExpression);
-      }
-      if (path.node.body.length === 0) {
-        path.node.body = [runTimeVariableExpression];
-      }
-      path.node.body.push(t.expressionStatement(
-        t.callExpression(
-          t.memberExpression(t.identifier('rtsTest'), t.identifier('summary')),
-          []
-        )
-      ))
-    }
-  },
-
-  CallExpression: {
-    exit(path, st: S) {
-      if (!st.opts.runTests) {
-        return;
-      }
-      if (t.isIdentifier(path.node.callee) && 
-      testingFunctions.includes(path.node.callee.name)) {
-        path.node.callee = t.memberExpression(
-          t.identifier('rtsTest'),
-          path.node.callee,
-        );
-      }
-      path.skip();
     }
   },
 
