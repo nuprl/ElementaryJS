8516299e58b64537f3a1328e0d3f9a5bf89c9a89
Arjun Guha <arjun@cs.umass.edu>
2018-08-07 14:43:01 -0400

    Minor cleanup

--- ts/bin.ts
+++ ts/bin.ts
@@ -1,8 +1,13 @@
+/**
+ * This file is the entrypoint for running ElementaryJS from the command-line.
+ * This is not how anyone will usually run ElementaryJS, but it is helpful
+ * for debugging.
+ */
 import * as babel from 'babel-core';
 import * as visitor from './visitor';
 import * as fs from 'fs';
 
-export function compile(code: string): string {
+function compile(code: string): string {
   const result = babel.transform(code, {
     plugins: [visitor.plugin],
     ast: false,
