d1f14c2bf3aeb42cb70c8a5f8ddb05e48d3111d4
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-10-04 18:47:36 -0400

    Move 'scripts' contents to 'eval'.

--- eval/fetchLibs.sh
+++ eval/fetchLibs.sh
@@ -2,6 +2,6 @@
 #WD: "/ElementaryJS/eval".
 
 mkdir libs
-curl -K ./scripts/libs.txt
+curl -K ./libs.txt
 echo "Libraries downloaded.
 You must edit them s.t. they can be 'required' by 'compileAndRun.js'."
