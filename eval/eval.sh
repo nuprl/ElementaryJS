b07d42d1a75cf141d35ce7155e905df62097ced4
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-10-07 19:40:48 -0400

    Pass flag for 'silent' mode.

--- eval/eval.sh
+++ eval/eval.sh
@@ -5,8 +5,8 @@
 # W/O: `time -p find ./userFiles/ -type f -name "*.js" -exec ./eval.sh {} \;`.
 
 if [[ -n $1 ]]; then
-  f="${1::-3}.log" # ATTN: Change to '_normal.log' or '_silent.log'.
-  if ! timeout 5 node ./compileAndRun.js "$1" &> "$f"; then
+  f="${1::-3}_normal.log" # NOTE: Change to '_silent.log'.
+  if ! timeout 5 node ./compileAndRun.js "$1" &> "$f"; then # NOTE: Pass a flag after "$1" to toggle mode.
     echo "TIMEOUT" >> "$f"
   fi
 fi
