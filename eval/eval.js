563c053ef0cd108f17cf42c8f07fe47a78018dee
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-03-09 23:27:14 -0400

    Move timeout from shell to JS.

--- eval/eval.js
+++ eval/eval.js
@@ -17,7 +17,8 @@ const readFile = require('fs').readFile,
       version = require('../dist/version.js'),
       lib220 = require('./libs/lib220.js'),
       oracle = require('./libs/oracle.js'),
-      rrt = require('./libs/rrt.js');
+      rrt = require('./libs/rrt.js'),
+      TIMEOUT = 10000; // ms
 
 if (process.argv.length < 3 || process.argv.length > 5) {
   console.error('Invalid number of arguments to \'eval\'.');
@@ -27,31 +28,34 @@ const input = process.argv[2];
 
 readFile(input, (e, f) => {
   if (e) { throw e; }
-  try {
-    const opts = {
-            consoleLog: s => console.log(s),
-            ejsOff: Boolean(process.argv[4]),
-            version: () => console.log(version.EJSVERSION),
-            whitelistCode: { lib220, oracle, rrt }
-          },
-          tOn = Boolean(process.argv[3]),
-          compileResult = ejs.compile(f.toString(), opts);
-    rt.enableTests(tOn);
+  const opts = {
+          consoleLog: s => console.log(s),
+          ejsOff: Boolean(process.argv[4]),
+          version: () => console.log(version.EJSVERSION),
+          whitelistCode: { lib220, oracle, rrt }
+        },
+        tOn = Boolean(process.argv[3]),
+        compileResult = ejs.compile(f.toString(), opts);
+  rt.enableTests(tOn);
 
-    if (compileResult.kind === 'error') {
-      throw `\n${compileResult.errors.map(e =>
-        `- ${e.message} (line ${e.line})`).join('\n')}`;
-    }
+  if (compileResult.kind === 'error') {
+    return console.error(`EXIT FAILURE on input ${input}:\n${compileResult.errors.map(e =>
+      `- ${e.message} (line ${e.line})`).join('\n')}`);
+  }
+
+  new Promise((resolve, reject) => {
+    const tId = global.setTimeout(() => reject('TIMEOUT'), TIMEOUT);
 
     compileResult.run(runResult => {
+      global.clearTimeout(tId);
+
       if (runResult.type === 'exception') {
-        throw `${runResult.value}\n\t${runResult.stack.join('\n\t')}`;
+        reject(`${runResult.value}\n\t${runResult.stack.join('\n\t')}`);
       }
-      console.log(
-        `${tOn ? `${rt.summary().output}\n` : ''}EXIT SUCCESS on input ${input}`
-      );
+      resolve(`${tOn ? `${rt.summary().output}\n` : ''}EXIT SUCCESS on input ${input}`);
     });
-  } catch (e) {
-    console.error(`EXIT FAILURE on input ${input}: ${e}`);
-  }
+  }).then(result => console.log(result), reason => {
+    console.error(`EXIT FAILURE on input ${input}: ${reason}`);
+    process.exit(1); // Note: This seems wrong.
+  });
 });
