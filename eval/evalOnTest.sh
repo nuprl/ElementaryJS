5b06ac130f621b9d763d08607e5f476c2bc42ec1
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-03-23 14:40:09 -0400

    Add hard T/O.

--- eval/evalOnTest.sh
+++ eval/evalOnTest.sh
@@ -5,6 +5,9 @@
 # W/O: `time find ./userFiles/ -type f -name "*.js" -exec ./evalOnTest.sh {} \;`.
 
 if [[ -n $1 ]]; then
+  f="${1::-3}_on_test.log"
   # NOTE: <test file> must be a '.js' file, else in-line tests will be run.
-  node ./eval.js "$1" "<test file>" &> "${1::-3}_on_test.log"
+  if ! timeout 120 node ./eval.js "$1" "<test file>" &> "$f"; then
+    echo "SIGTERM" >> "$f"
+  fi
 fi
