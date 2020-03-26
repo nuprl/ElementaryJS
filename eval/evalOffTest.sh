909bf62e573815b5150aea6e05802c5e7db035e7
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-03-26 15:49:48 -0400

    Format test scripts correctly.

--- eval/evalOffTest.sh
+++ eval/evalOffTest.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #WD: "/ElementaryJS/eval".
 
-# W/ GNU PARALLEL: `time find ./userFiles/ -type f -name "*.js" | parallel ./evalOffTest.sh`.
-# W/O: `time find ./userFiles/ -type f -name "*.js" -exec ./evalOffTest.sh {} \;`.
+# W/ GNU PARALLEL: `time sed 's/^./userFiles/' userFiles/tests/a<#>.txt | parallel ./evalOffTest.sh {} userFiles/tests/a<#>.js`.
+# W/ <#> as the assignment number. Assumes `hwFiles.sh` has been run.
 
 if [[ -n $1 ]]; then
   f="${1::-3}_off_test.log"
-  # NOTE: <test file> must be a '.js' file, else in-line tests will be run.
-  if ! timeout 120 node ./eval.js "$1" "<test file>" 1 &> "$f"; then
+  tf="in-line"
+  if [[ -n $2 ]]; then
+    tf="$2" # separate file
+  fi
+  if ! timeout 120 node ./eval.js "$1" "$tf" 1 &> "$f"; then
     echo "SIGTERM" >> "$f"
   fi
 fi
--- eval/evalOnTest.sh
+++ eval/evalOnTest.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #WD: "/ElementaryJS/eval".
 
-# W/ GNU PARALLEL: `time find ./userFiles/ -type f -name "*.js" | parallel ./evalOnTest.sh`.
-# W/O: `time find ./userFiles/ -type f -name "*.js" -exec ./evalOnTest.sh {} \;`.
+# W/ GNU PARALLEL: `time sed 's/^./userFiles/' userFiles/tests/a<#>.txt | parallel ./evalOnTest.sh {} userFiles/tests/a<#>.js`.
+# W/ <#> as the assignment number. Assumes `hwFiles.sh` has been run.
 
 if [[ -n $1 ]]; then
   f="${1::-3}_on_test.log"
-  # NOTE: <test file> must be a '.js' file, else in-line tests will be run.
-  if ! timeout 120 node ./eval.js "$1" "<test file>" &> "$f"; then
+  tf="in-line"
+  if [[ -n $2 ]]; then
+    tf="$2" # separate file
+  fi
+  if ! timeout 120 node ./eval.js "$1" "$tf" &> "$f"; then
     echo "SIGTERM" >> "$f"
   fi
 fi
