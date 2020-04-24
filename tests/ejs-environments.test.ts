962e50847d453997befbee71999682d6880c4ed1
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-04-24 00:39:00 -0400

    [skip ci] Assignment expression (update env); use Babel API checks.

--- tests/ejs-environments.test.ts
+++ tests/ejs-environments.test.ts
@@ -1,30 +1,51 @@
 import { compileOK, staticError } from './test-utils';
 
 describe('ElementaryJS Environments', () => {
+  const x: string = `You must initialize the variable 'x' before use.`;
+
   test('Trivial uninitialized let (+)', () => {
     compileOK(`let x;`);
     compileOK(`let x; x = 1;`);
+    compileOK(`let x; x = 1; x;`);
+    compileOK(`let x, y; x = undefined; y = x;`);
   });
 
   test('Trivial uninitialized let (-)', () => {
-    expect(staticError(`let x; x;`)).toEqual(
-      expect.arrayContaining([
-        `You must initialize the variable 'x' before use.`
-      ]));
-    expect(staticError(`let x, y = x;`)).toEqual(
-      expect.arrayContaining([
-        `You must initialize the variable 'x' before use.`
-      ]));
+    expect(staticError(`let x; x;`)).toEqual(expect.arrayContaining([x]));
+    expect(staticError(`let x, y = x;`)).toEqual(expect.arrayContaining([x]));
+  });
+
+  test('Array Expression (+)', () => {
+    compileOK(`let x; x = '1'; [x];`);
+  });
+
+  test('Array Expression (-)', () => {
+    expect(staticError(`let x; [x];`)).toEqual(expect.arrayContaining([x]));
+  });
+
+  test('Uniary operator (+)', () => {
+    compileOK(`let x; x = '1'; +x;`);
+  });
+
+  test('Uniary operator (-)', () => {
+    expect(staticError(`let x; +x;`)).toEqual(expect.arrayContaining([x]));
+  });
+
+  test('Update expression (+)', () => {
+    compileOK(`let x; x = 1; ++x;`);
+  });
+
+  test('Update expression (-)', () => {
+    expect(staticError(`let x; ++x;`)).toEqual(expect.arrayContaining([x]));
   });
 
   test('Basic shadowing (+)', () => {
-    compileOK(`let x; {let x = 1; x;}`);
+    compileOK(`let x; { let x = 1; x; }`);
   });
 
   test('Basic shadowing (-)', () => {
-    expect(staticError(`let x; {let x = 1; x;} x;`)).toEqual(
-      expect.arrayContaining([
-        `You must initialize the variable 'x' before use.`
-      ]));
+    expect(staticError(`let x; {
+      let x = 1; x;
+    } x;`)).toEqual(expect.arrayContaining([x]));
   });
 });
